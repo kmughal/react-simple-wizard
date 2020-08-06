4ace25ff076f7c2b27e3440cb9da00ff6ebbec43
kmughal <kmughal@gmail.com>
2020-08-06 15:44:25 +0100

    feat(Wizard) Update Wizard component after adding new props

--- bin/Wizard.tsx
+++ bin/Wizard.tsx
@@ -1,28 +1,77 @@
 import React from 'react';
-import { StepsProps, NavigationsProps } from '.';
+import { StepsProps, NavigationsProps, WizardProps } from '.';
 import Navigations from './Navigations';
 
-const Wizard = (components: Array<React.FC<StepsProps>>) => {
+const Wizard: React.FC<{ wizardProps: WizardProps }> = ({ wizardProps }) => {
+  const totalSteps = wizardProps.steps.length;
+
   const [current, setCurrent] = React.useState(0);
+  const [showNextLink, setShowNextLink] = React.useState(true);
+  const [showPreviousLink, setShowPreviousLink] = React.useState(false);
 
   const states = [];
-  components.forEach((_) => states.push(React.useState(null)));
+  wizardProps.steps.forEach((_) => states.push(React.useState(null)));
 
   const [state, setState] = states[current];
 
-  const componentArg: StepsProps = { state, setState };
-  const component = components[current];
+  const goToStep = (stepIndex: number) => {
+    if (stepIndex < 0) {
+      console.error(`${stepIndex} is not valid.It must be >=0`);
+      return;
+    } else if (stepIndex >= wizardProps.steps.length) {
+      console.error(`${stepIndex} is not valid. It must be >${totalSteps - 1}`);
+      return;
+    }
+    if (stepIndex >= totalSteps - 1) {
+      setShowNextLink(false);
+      setShowPreviousLink(true);
+    } else if (stepIndex <= 0) {
+      setShowNextLink(true);
+      setShowPreviousLink(false);
+    } else {
+      setShowNextLink(true);
+      setShowPreviousLink(true);
+    }
+
+    setCurrent(stepIndex);
+  };
+
+  const getAllStates = (): Array<Record<string, any>> => {
+    const result = new Array<Record<string, any>>();
+
+    states.forEach((item, index) => {
+      result.push({ [`step${index}`]: item[0] });
+    });
+
+    return result;
+  };
+
+  const stepArgs: StepsProps = { state, setState, goToStep, getAllStates };
+  const step = wizardProps.steps[current];
   const navigationsArgs: NavigationsProps = {
     current,
     setCurrent,
-    totalComponents: components.length,
+    totalComponents: totalSteps,
+    showNextLink,
+    showPreviousLink,
+    goToStep,
   };
 
   return (
-    <>
-      <Navigations navigationsProps={navigationsArgs} />
-      {component(componentArg)}
-    </>
+    <section>
+      {wizardProps.showHeading && wizardProps.heading && (
+        <h1>{wizardProps.heading}</h1>
+      )}
+      {wizardProps.showNavigationLinks &&
+        wizardProps.showNavigationOrientation === 'top' && (
+          <Navigations navigationsProps={navigationsArgs} />
+        )}
+      {step(stepArgs)}
+      {wizardProps.showNavigationLinks &&
+        wizardProps.showNavigationOrientation === 'bottom' && (
+          <Navigations navigationsProps={navigationsArgs} />
+        )}
+    </section>
   );
 };
 
